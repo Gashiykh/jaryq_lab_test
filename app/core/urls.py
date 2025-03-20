356497035cc6a039acdb0c1ce1ce92b6ebcc9b0f
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-21 00:18:47 +0500

    Подключил все url пути, а также добавил swagger

--- app/core/urls.py
+++ app/core/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Restaurant Booking API",
+      default_version='v1',
+      description="API для бронирования столиков",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("", include("webapp.urls")),
+    path("api/", include("api.urls")),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
