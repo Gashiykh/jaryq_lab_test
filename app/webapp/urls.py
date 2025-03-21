937940d63da65a978215ba638923a459057cfb94
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-21 11:22:53 +0500

    Добавил url пути для view

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -4,8 +4,5 @@ from webapp import views
 
 urlpatterns = [
     path('', views.RedirectView.as_view(), name='redirect'),
-    path('reservations', views.ReservationListView.as_view(), name='reservations'),
-    path('reservations/add', views.ReservationCreateView.as_view(), name='reservations_add'),
-    path('reservations/<int:id>', views.ReservationDetailView.as_view(), name='reservations_detail'),
-    path('reservations/<int:id>/delete', views.ReservationDeleteView.as_view(), name='reservations_delete'),
+    path('seating/', views.SeatingChartView.as_view(), name='reservation_chart'),
 ]
